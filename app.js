9e280bb8876cc2737713feaa919befbc70c6bc21
AltomDevelopment <Zack.Altom@outlook.com>
2022-05-16 14:44:27 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+
+function addNote() {
+   let textBox = document.createElement('textarea');
+   document.body.appendChild(textBox);
+}
+
+function addImage() {
+    let imageUrlBox = document.createElement('input');  
+    imageUrlBox.setAttribute('value', 'Paste Image URL');
+    document.body.appendChild(imageUrlBox);
+    let imageSubmitButton = document.createElement('button');
+    imageSubmitButton.innerHTML = 'Submit';
+    document.body.appendChild(imageSubmitButton);
+
+    imageUrlBox.onclick = function () {
+        imageUrlBox.removeAttribute('value', '');
+    };
+
+    imageSubmitButton.onclick = function () {
+        document.body.removeChild(imageUrlBox);
+        document.body.removeChild(imageSubmitButton);
+        let createImage = document.createElement('img');
+        createImage.src = imageUrlBox.value; 
+        document.body.appendChild(createImage);
+    }
+};
+
+
+function addVoice() {
+
+}
+
